29ac2833969c95523c8848eebb1bd507376e6870
tjdals5567 <84655119+tjdals5567@users.noreply.github.com>
2023-02-16 21:11:43 +0900

    Create 1463.py

---
+++ DP/백준_1463/1463.py
@@ -0,0 +1,10 @@
+a = int(input())
+n = [0]*(a+1)
+for i in range(2,a+1):
+    n[i] = n[i-1] + 1
+    if i%2 == 0:
+        n[i] = min(n[i],n[i//2]+1) 
+    if i%3 == 0:
+        n[i] = min(n[i],n[i//3]+1) 
+
+print(n[a])
