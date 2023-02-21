39f3f8fdb305cdcf9d7138818aa7fa49f8196517
tjdals5567 <84655119+tjdals5567@users.noreply.github.com>
2023-02-21 12:19:35 +0900

    Create 10844.py

---
+++ DP/백준_10844_쉬운 계단 수/10844.py
@@ -0,0 +1,13 @@
+n = int(input())
+dp = [[0 for i in range(10)] for i in range(n+1)]
+dp[1] = [0,1,1,1,1,1,1,1,1,1]
+for i in range(2,n+1):
+    for j in range(10):
+        if j-1 < 0 :
+            dp[i][j] = dp[i-1][j+1] % 1000000000
+        elif j + 1 > 9:
+            dp[i][j] = dp[i-1][j-1] % 1000000000
+        else:
+            dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1000000000
+
+print(sum(dp[n])%1000000000)
